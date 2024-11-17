62006bbcf34a8039af807f50f33600c235b92e2a
hacktic <foorganders@outlook.com>
2024-11-17 19:53:22 +0100

    Add shears tutorial

--- Assets/UI/ShearsTutorial.lua
+++ Assets/UI/ShearsTutorial.lua
@@ -19,25 +19,24 @@ local playerManager = require("PlayerManager")
 function Init()
     page = 0
     closeLabel:SetPrelocalizedText("Next")
-    _tutorial1:SetPrelocalizedText("Shears tutorial test 1")
-    _tutorialImage:RemoveFromClassList("bee-image")
-    _tutorialImage:AddToClassList("shopkeeper-image")
+    _tutorial1:SetPrelocalizedText("Congratulations on your new shears! Keep an eye out for flowers growing that you can pick.")
+    _tutorialImage:AddToClassList("image-1")
 end
 
 function self:ClientAwake()
 
     closeButton:RegisterPressCallback(function()
         if page == 0 then
-            _tutorial1:SetPrelocalizedText("Shears tutorial test 2")
+            _tutorial1:SetPrelocalizedText("Once you've got some flowers, you can plant them in your apiary to increase your honey generation.")
             closeLabel:SetPrelocalizedText("Next")
-            _tutorialImage:RemoveFromClassList("shopkeeper-image")
-            _tutorialImage:AddToClassList("baybee-image")
+            _tutorialImage:RemoveFromClassList("image-1")
+            _tutorialImage:AddToClassList("image-2")
             page = 1
         elseif page == 1 then
-            _tutorial1:SetPrelocalizedText("Shears tutorial test 3")
+            _tutorial1:SetPrelocalizedText("Each type of flower has a different effect, so mix and match them to find the best combination for you!")
             closeLabel:SetPrelocalizedText("Close")
-            _tutorialImage:RemoveFromClassList("baybee-image")
-            _tutorialImage:AddToClassList("bee-image")
+            _tutorialImage:RemoveFromClassList("image-3")
+            _tutorialImage:AddToClassList("image-3")
             page = 2
         elseif page == 2 then
             UIManager.CloseShearsTutorial()
