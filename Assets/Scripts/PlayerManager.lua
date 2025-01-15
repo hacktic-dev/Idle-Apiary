b1e7ad9fc6055876206f35651a6dd73d61f498a8
hacktic <foorganders@outlook.com>
2025-01-15 20:25:53 +0100

    Fix unable to scroll hat menu

--- Assets/Scripts/PlayerManager.lua
+++ Assets/Scripts/PlayerManager.lua
@@ -486,7 +486,7 @@ function TrackPlayers(game, characterCallback)
             beeObjectManager.SpawnAllBeesForPlayer(player)
             flowerManager.SpawnAllFlowersForIncomingPlayer(player)
             playerTimers[player] = nil
-            setPlayerVersionString:FireClient(player, "1.3.4")
+            setPlayerVersionString:FireClient(player, "1.3.5")
             --festiveBeeManager.OnPlayerJoined(player)
 
             for player, playerData in pairs(players) do
