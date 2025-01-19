2df4f91b168efa91d447ff3765d1975d9648ab42
hacktic <foorganders@outlook.com>
2025-01-19 19:06:35 +0100

    Set flower spawn rate back to normal

--- Assets/Scripts/FlowerManager.lua
+++ Assets/Scripts/FlowerManager.lua
@@ -23,7 +23,7 @@ local apiaryManager = require("ApiaryManager")
 giveFlower = Event.new("giveFlowerEvent")
 
 function self:ClientAwake()
-    Timer.new(.1, function() TrySpawnFlower() end, true)
+    Timer.new(35, function() TrySpawnFlower() end, true)
 end
 
 local MIN_SPAWN_DISTANCE = 45 
