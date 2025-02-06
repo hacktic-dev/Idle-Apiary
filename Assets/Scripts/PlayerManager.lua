70f98505e9ce8b89a633ce5b2c8c7e18243952e8
hacktic <foorganders@outlook.com>
2025-02-06 10:36:43 +0100

    Update version number to 1.4.1

--- Assets/Scripts/PlayerManager.lua
+++ Assets/Scripts/PlayerManager.lua
@@ -506,7 +506,7 @@ function TrackPlayers(game, characterCallback)
             beeObjectManager.SpawnAllBeesForPlayer(player)
             flowerManager.SpawnAllFlowersForIncomingPlayer(player)
             playerTimers[player] = nil
-            setPlayerVersionString:FireClient(player, "1.4.0")
+            setPlayerVersionString:FireClient(player, "1.4.1")
             romanticBeeManager.OnPlayerJoined(player)
 
             for player, playerData in pairs(players) do
